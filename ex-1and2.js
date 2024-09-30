3adb82438a5bb7d70d10813585d3ee8ad82a0272
Thanate Dokmai <nook.microstock@gmail.com>
2024-09-30 13:42:13 +0700

    feat: assign1-Nook

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,19 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restuarantName = "Chef's Table by Chef James";
+const netBill = 15000;
+const totalBranchRestuarant = 1500;
+
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+netBill = 5000; //เป็น const ไม่สามารถ reassigned ได้
+totalBranchRestuarant = 1500; //เป็น const ไม่สามารถ reassigned ได้
+
+
+
+
+
