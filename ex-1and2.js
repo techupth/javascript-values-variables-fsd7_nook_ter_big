c9971d9256b9af65051b72ddf89f116d472d6c41
TerSi <ter.sirawit1997@gmail.com>
2024-09-30 13:22:09 +0700

    assingment values and variables in javascript

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+// Ter
+let firstName = "Karter";
+let lastName = "Anderson";
+let userAddress = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 
+const nameRestaurant = "Chef's Table by Chef James";
+const totalBill = 15000;
+const totalOpenRestaurant = 1500;
 //Exercise #2: Variable Reassignment
 //Start coding here
+userAddress = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+totalBill = 5000;
+totalOpenRestaurant = 2500;
